53df74dafbf3329fca0fb696a3ff73e1d49e69b7
Rigel <rigel.rahateen@student.president.ac.id>
2024-07-20 21:03:23 +0700

    library clean up

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
-const { GoogleAuth } = require('google-auth-library');
-const rateLimit = require('express-rate-limit');
 
 const express = require('express');
 const axios = require('axios');
@@ -11,10 +9,6 @@ const port = 5000;
 app.use(express.json());
 
 const GENIUS_ACCESS_TOKEN = process.env.REACT_APP_GENIUS_ACCESS_TOKEN;
-
-// Rate limiting configuration
-const characterLimit = 100000; // 100,000 characters
-const minute = 60 * 1000; // 1 minute in milliseconds
 
 app.get('/api/search', async (req, res) => {
     const { searchTerm } = req.query;
@@ -61,73 +55,6 @@ app.get('/api/lyrics', async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Error fetching lyrics' });
-    }
-});
-
-async function getAccessToken() {
-    const auth = new GoogleAuth({
-        keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-        scopes: ['https://www.googleapis.com/auth/cloud-translation']
-    });
-
-    const client = await auth.getClient();
-    const accessToken = await client.getAccessToken();
-    return accessToken.token;
-}
-
-const limiter = rateLimit({
-    windowMs: minute,
-    max: characterLimit, // Limit each IP to characterLimit requests per `window` (here, per minute)
-    keyGenerator: (req) => {
-        return req.ip; // Use the user's IP address as the key
-    },
-    handler: (req, res) => {
-        res.status(429).json({ error: 'Rate limit exceeded. Try again later.' });
-    },
-    // Custom function to count the characters in the request
-    keyLength: (req) => {
-        return req.body.text.length;
-    }
-});
-
-// Endpoint to romanize text
-// Apply the rate limiter to the romanize route
-app.post('/api/romanize', limiter, async (req, res) => {
-    const { text, sourceLanguage } = req.body;
-    const projectId = 'utacy-lyrics'; // Replace with your project ID
-    const location = 'global'; // The location for Indonesia
-
-    try {
-        const accessToken = await getAccessToken();
-        const url = `https://translation.googleapis.com/v3/projects/${projectId}/locations/${location}:romanizeText`;
-
-        // Split text by newlines to preserve them
-        const lines = text.split('\n');
-        const romanizedLines = [];
-
-        for (const line of lines) {
-            if (line.trim() === '') {
-                romanizedLines.push(''); // Preserve empty lines
-                continue;
-            }
-            const response = await axios.post(url, {
-                source_language_code: sourceLanguage,
-                contents: [line]
-            }, {
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            });
-
-            const romanizedText = response.data.romanizations[0].romanizedText;
-            romanizedLines.push(romanizedText);
-        }
-
-        const romanizedText = romanizedLines.join('\n');
-        res.json({ romanizedText });
-    } catch (error) {
-        console.error('Error romanizing text:', error.response ? error.response.data : error.message);
-        res.status(500).json({ error: 'Error romanizing text' });
     }
 });
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,20 @@ import keyIcon from '../img/key.svg';
 import questionIcon from '../img/help-circle.svg';
 import KeyGuide from './KeyGuide';
 import About from './About';
+import logo from '../img/logo.jpg';
+
 
 import '../App.css';
 
 const Header = ({ openModal }) => (
   <header className="header">
-    <h1 className="text-1xl font-bold">UtacyLyrics</h1>
+
+    <div className='flex flex-row items-center'>
+      <img src={logo} alt="logo" className="w-8 rounded-lg"></img>
+      <h1 className="text-1xl font-bold ml-1">UtacyLyrics</h1>
+
+    </div>
+   
     <div className="flex space-x-4">
       <button onClick={() => openModal('API Keys', <KeyGuide />)}>
         <img src={keyIcon} alt="First Button" className="w-6 h-6 invert transition ease-in-out hover:scale-110 duration-200"  />
